ec9360eb70c4f109cf828cd9fdf0a959ecf0ae47
Douglas Castro <douglashenriquees@gmail.com>
2022-09-10 12:50:04 -0300

    update Publisher/Program.cs

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+
+// Definição da conexão
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+// Conexão com um NÓ RabbitMQ
+using (var connection = factory.CreateConnection())
+{
+    // Canal para definição de uma fila para publicar mensagens
+    using (var channel = connection.CreateModel())
+    {
+
+    }
+}
