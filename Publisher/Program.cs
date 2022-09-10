e5657e18b6fc1731b46596c18cf3d3d897ff0ce0
Douglas Castro <douglashenriquees@gmail.com>
2022-09-10 14:10:46 -0300

    update Publisher.csproj

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using System.Text;
+using RabbitMQ.Client;
 
 // Definição da conexão
 var factory = new ConnectionFactory()
@@ -12,6 +13,18 @@ using (var connection = factory.CreateConnection())
     // Canal para definição de uma fila para publicar mensagens
     using (var channel = connection.CreateModel())
     {
+        channel.QueueDeclare(
+            "saudacao_1", // nome da fila
+            false, // ativo após o servidor ser reiniciado?
+            false, // só pode ser acessada na conexão atual?
+            false, // é removida após os consumidores utilizarem a fila?
+            null);
 
+        var message = "Bem-vindo ao RabbitMQ";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "saudacao_1");
+
+        Console.WriteLine($" [X] Enviada: {message}");
     }
 }
--- README.md
+++ README.md
@@ -13,4 +13,4 @@
 
 ## Executando o Container do RabbitMQ
 
-* ```docker run -d -p 15672:15672 rabbitmq:3-management```
+* ```docker run -d -p 5672:5672 -p 15672:15672 rabbitmq:3-management```
